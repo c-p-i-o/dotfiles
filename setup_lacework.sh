278d36f33c5adb48f51f353afa6e6c0ab5fde5a5
Nick Schmeller <nick.schmeller@lacework.net>
2023-01-26 16:51:38 +0000

    feat: setup git for Lacework
    Signed-off-by: Nick Schmeller <nick.schmeller@lacework.net>

--- setup_lacework.sh
+++ setup_lacework.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
 # set GOPATH
-echo "Setting GOPATH to HOME/lwcode"
-LACEWORK_CODE=lwcode
-export GOPATH=$HOME/$LACEWORK_CODE
+echo "Setting GOPATH to ~/lwcode"
+export GOPATH=$HOME/lwcode
 
 # setup work directories
 echo "Setting up work directories"
@@ -11,3 +10,8 @@ LACEWORK_DIR=$GOPATH/src/github.com/lacework
 LACEWORK_DEV_DIR=$GOPATH/src/github.com/lacework-dev
 mkdir -p $LACEWORK_DIR
 mkdir -p $LACEWORK_DEV_DIR
+
+# Lacework-specific git settings
+CODER_SSH_PUBKEY=$HOME/.ssh/coder.pub
+if [ -e "$CODER_SSH_PUBKEY" ]; then git config --global user.signingkey $CODER_SSH_PUBKEY; fi
+if [ -z "$CODER_USER_EMAIL" ]; then git config --global user.email $CODER_USER_EMAIL; fi
