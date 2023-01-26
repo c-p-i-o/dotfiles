3de0fbf8b44fe2c8966a589c0bf25f45d36cf4c1
nschmeller <nick.schmeller@lacework.net>
2023-01-26 04:47:29 +0000

    refactor: use variables in `setup_lacework.sh`
    Signed-off-by: nschmeller <nick.schmeller@lacework.net>

--- setup_lacework.sh
+++ setup_lacework.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
-# setup work directories
-echo "Setting up work directories"
-mkdir -p $HOME/lwcode/src/github.com/lacework/agent
-mkdir -p $HOME/lwcode/src/github.com/lacework-dev
-
 # set GOPATH
 echo "Setting GOPATH to HOME/lwcode"
-export GOPATH=$HOME/lwcode
+LACEWORK_CODE=lwcode
+export GOPATH=$HOME/$LACEWORK_CODE
+
+# setup work directories
+echo "Setting up work directories"
+LACEWORK_DIR=$GOPATH/src/github.com/lacework
+LACEWORK_DEV_DIR=$GOPATH/src/github.com/lacework-dev
+mkdir -p $LACEWORK_DIR
+mkdir -p $LACEWORK_DEV_DIR
