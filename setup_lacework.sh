276c518aaeb334fb336a7275ecd6ff3ea6c45a65
Nick Schmeller <26189705+nschmeller@users.noreply.github.com>
2023-02-06 21:06:35 +0000

    style: use double brackets in `setup_lacework.sh`
    Signed-off-by: Nick Schmeller <26189705+nschmeller@users.noreply.github.com>

--- setup_lacework.sh
+++ setup_lacework.sh
@@ -12,5 +12,5 @@ mkdir -p $LACEWORK_DEV_DIR
 
 # Lacework-specific git settings
 CODER_SSH_PUBKEY=$HOME/.ssh/coder.pub
-if [ -e "$CODER_SSH_PUBKEY" ]; then git config --global user.signingkey $CODER_SSH_PUBKEY; fi
+if [[ -e "$CODER_SSH_PUBKEY" ]]; then git config --global user.signingkey $CODER_SSH_PUBKEY; fi
 if [[ -v CODER_USER_EMAIL ]]; then git config --global user.email $CODER_USER_EMAIL; fi
